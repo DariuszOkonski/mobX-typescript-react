ac8b04308f85c4834603dc6d1a3b2d515084271a
Dariusz Okonski <darek200180@gmail.com>
2022-08-22 17:42:17 +0100

    add CommentsStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
-import StoreProvider from './stores/StoreProvider';
-import Forms from './Forms';
-import Comments from './Comments';
 
 function App() {
   return (
-    <StoreProvider>
-      <div className="App">
-        <header className="App-header">
-          <h1>MobX</h1>
-          <Forms />
-          <Comments />
-        </header>
-      </div>
-    </StoreProvider>
+    <div className="App">
+      <header className="App-header">
+        <h1>MobX</h1>
+      </header>
+    </div>
   );
 }
 
--- src/stores/RootStore.ts
+++
@@ -1,9 +0,0 @@
-import CommentStore from "./CommentsStore";
-
-export default class RootStore {
-    commentStore: CommentStore;
-
-    constructor() {
-        this.commentStore = new CommentStore();
-    }
-}
